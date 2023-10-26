c23368f8e90060e897e204dc7c199a968c6ad3d9
Peter Teuben <teuben@gmail.com>
2023-10-25 20:51:36 -0400

    upd pix_list

--- mk_runs.py
+++ mk_runs.py
@@ -19,7 +19,7 @@ pars1['NGC6946']   = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['NGC6946']   = "srdp=1 sdfits=1 admit=0"
+pars2['NGC6946']   = "pix_list=-0,5  srdp=1 sdfits=1 admit=0"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, sys.argv)
