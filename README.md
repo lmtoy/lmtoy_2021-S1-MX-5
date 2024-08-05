9a24891b82e10bec4dfb8015e326a1db78cf3252
Peter Teuben <teuben@gmail.com>
2024-08-04 20:52:54 -0400

    upd

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# 2023-S1-US-18
+# 2021-S1-MX-5
 
-this project ....
+this project observed NGC6946 in two obsnums. Clear detection of CO(1-0)
+covering from about -80 to +190.   vlsr=40 from header
+Signal present to the edges,  
--- mk_runs.py
+++ mk_runs.py
@@ -15,11 +15,11 @@ on['NGC6946'] = [100632, 100633 ]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['NGC6946']   = ""
+pars1['NGC6946']   = "dv=150 dw=200 b_order=1"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['NGC6946']   = "pix_list=-0,5  srdp=1 sdfits=1 admit=0"
+pars2['NGC6946']   = "pix_list=-0,5"
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, None, sys.argv)
